809361272a808833b0f00f9979766dbdbd08b523
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2023-06-24 15:26:12 +0600

    add input form

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@
 function App() {
 
   return (
-    <>
-      tg
-    </>
+    <div className="input-form">
+      <input type="text" />
+      <select name="" id="">
+        <option value="">test</option>
+        <option value="">test</option>
+        <option value="">test</option>
+      </select>
+      <input type="number" />
+      <input type="number" />
+      <input type="date" />
+      <textarea name="" id="" cols={30} rows={2}>
+
+      </textarea>
+    </div>
   )
 }
 
