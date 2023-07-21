7d2c8fca125d16313f61230cb786d134509f0bd2
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2023-07-21 16:23:09 +0600

    delete swip

--- src/App.tsx
+++ src/App.tsx
@@ -111,56 +111,6 @@ const App = () => {
     }));
   };
 
-  // Функция, которая ограничивает значения часов до 24
-  const handleHourChange = (event: ChangeEvent<HTMLInputElement>) => {
-    let value = parseInt(event.target.value, 10);
-    value = Math.min(Math.max(value, 0), 24);
-    setFormData((prevData) => ({
-      ...prevData,
-      hour: value.toString().padStart(2, '0'), // Добавляем ведущий ноль для однозначных чисел (например, 5 => 05)
-    }));
-  };
-
-  // Функция, которая ограничивает значения минут до 60
-  const handleMinuteChange = (event: ChangeEvent<HTMLInputElement>) => {
-    let value = parseInt(event.target.value, 10);
-    value = Math.min(Math.max(value, 0), 59);
-    setFormData((prevData) => ({
-      ...prevData,
-      minute: value.toString().padStart(2, '0'), // Добавляем ведущий ноль для однозначных чисел (например, 5 => 05)
-    }));
-  };
-
-  // Обработчик окончания тяги для часов
-  const handleHourDragEnd = (event: React.TouchEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    const startY = parseInt(event.currentTarget.dataset.startY || '0', 10);
-    const endY = event.changedTouches[0].clientY;
-    const diff = startY - endY;
-    const increment = diff > 0 ? 1 : -1;
-    let value = parseInt(formData.hour, 10) + increment;
-    value = Math.min(Math.max(value, 0), 23);
-    setFormData((prevData) => ({
-      ...prevData,
-      hour: value.toString().padStart(2, '0'),
-    }));
-  };
-
-  // Обработчик окончания тяги для минут
-  const handleMinuteDragEnd = (event: React.TouchEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    const startY = parseInt(event.currentTarget.dataset.startY || '0', 10);
-    const endY = event.changedTouches[0].clientY;
-    const diff = startY - endY;
-    const increment = diff > 0 ? 5 : -5;
-    let value = parseInt(formData.minute, 10) + increment;
-    value = Math.min(Math.max(value, 0), 59);
-    setFormData((prevData) => ({
-      ...prevData,
-      minute: value.toString().padStart(2, '0'),
-    }));
-  };
-
   return (
     <div className="input-form">
       <div style={{
@@ -198,23 +148,20 @@ const App = () => {
       </select>
       <div className='time-form'>
         <input
-          placeholder="hours"
+          placeholder="00"
           type="number"
           name="hour"
+          max="23"
           value={formData.hour}
-          // onTouchMove={handleTouchMove} 
-          onChange={handleHourChange}
-          onTouchStart={(event) => event.currentTarget.dataset.startY = event.touches[0].clientY.toString()}
-          onTouchEnd={handleHourDragEnd}
+          onChange={handleChange}
         />
         <input
           type="number"
           name="minute"
-          placeholder="minutes"
+          placeholder="00"
+          max="59"
           value={formData.minute}
-          onChange={handleMinuteChange}
-          onTouchStart={(event) => event.currentTarget.dataset.startY = event.touches[0].clientY.toString()}
-          onTouchEnd={handleMinuteDragEnd}
+          onChange={handleChange}
         />
       </div>
       <input
