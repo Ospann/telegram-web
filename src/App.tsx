b9134d9b63703893bbee58f1257420b2af04583b
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2023-07-21 14:35:20 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,6 @@ const App = () => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        setMessage(formData.hour);
         resetFormData();
         return response.json();
       })
