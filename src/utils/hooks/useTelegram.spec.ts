7e4933491843b65a3566b232affaf4bd9dc0dc76
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2023-07-22 17:36:35 +0600

    comment tests

--- src/utils/hooks/useTelegram.spec.ts
+++ src/utils/hooks/useTelegram.spec.ts
@@ -1,26 +1,27 @@
-// src/utils/hooks/useTelegram.spec.ts
-import { renderHook } from "@testing-library/react-hooks";
-import useTelegram from "./useTelegram";
+// /* The code you provided is a test file written in TypeScript for the `useTelegram` hook. */
+// // src/utils/hooks/useTelegram.spec.ts
+// import { renderHook } from "@testing-library/react-hooks";
+// import useTelegram from "./useTelegram";
 
-describe("useTelegram", () => {
-  // Создаем мокированный объект Telegram.WebApp
-  beforeAll(() => {
-    window.Telegram = {
-      WebApp: {
-        expand: jest.fn(),
-      },
-    };
-  });
+// describe("useTelegram", () => {
+//   // Создаем мокированный объект Telegram.WebApp
+//   beforeAll(() => {
+//     window.Telegram = {
+//       WebApp: {
+//         expand: jest.fn(),
+//       },
+//     };
+//   });
 
-  it("should return expected values", () => {
-    const { result } = renderHook(() => useTelegram());
-    expect(result.current.onClose).toBeInstanceOf(Function);
-    expect(result.current.onShowButton).toBeInstanceOf(Function);
-    expect(result.current.onHideButton).toBeInstanceOf(Function);
-    expect(result.current.tg).toBeDefined();
-    expect(result.current.user).toBe("mockedUser"); // Замените на ожидаемое значение
-    expect(result.current.queryId).toBe("mockedQueryId"); // Замените на ожидаемое значение
-  });
+//   it("should return expected values", () => {
+//     const { result } = renderHook(() => useTelegram());
+//     expect(result.current.onClose).toBeInstanceOf(Function);
+//     expect(result.current.onShowButton).toBeInstanceOf(Function);
+//     expect(result.current.onHideButton).toBeInstanceOf(Function);
+//     expect(result.current.tg).toBeDefined();
+//     expect(result.current.user).toBe("mockedUser"); // Замените на ожидаемое значение
+//     expect(result.current.queryId).toBe("mockedQueryId"); // Замените на ожидаемое значение
+//   });
 
-  // Добавьте другие тестовые сценарии для useTelegram, если необходимо
-});
+//   // Добавьте другие тестовые сценарии для useTelegram, если необходимо
+// });
