7f232b2794bdd31882169bb8e036da3e79636291
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2023-07-22 17:29:44 +0600

    add validation

--- src/App.tsx
+++ src/App.tsx
@@ -118,8 +118,8 @@ const App = () => {
     HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
   >) => {
     const { name, value } = target;
-    if (name === 'hour' && Number(value) > 23) return;
-    if (name === 'minute' && Number(value) > 59) return;
+    if (name === 'hour' && (Number(value) > 23 || Number(value) < 0)) return;
+    if (name === 'minute' && (Number(value) > 59 || Number(value) < 0)) return;
 
     setFormData((prevData) => ({
       ...prevData,
